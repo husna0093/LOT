2122098bbc5b03739b369c3b1e1ca6809bf3f5eb
unknown <husna0093>
2020-06-23 00:28:30 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/psu/commsci/lot/MainActivity.kt
@@ -0,0 +1,20 @@
+package th.ac.psu.commsci.lot
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.ButtonBarLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
